a76b7903f93c2dd6f38e1c1cb0e7c221231e25ee
Kazuki Murahama <kazuki.murahama@gmail.com>
2015-10-18 14:42:34 +0900

    tags show ページデザイン調整

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,7 @@ class TagsController < ApplicationController
   skip_before_action :authenticate_user!
   def show
     @tag = Tag.find(params[:id])
+    @tips = Tip.page(params[:page]).tagged_with([@tag.name], any: :true)
     if user_signed_in?
       @tag_follow = current_user.tag_follows.find_by(tag_id: params[:id])
     end
